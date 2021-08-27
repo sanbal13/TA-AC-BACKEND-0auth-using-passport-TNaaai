90022b11c60f49e97f5d4c4c2e6640395391cbd1
Santosh Balchandran <santoshbalchandran@gmail.com>
2021-08-27 22:39:01 +0530

    Assignment-2 OAuth using Passport

---
+++ block-BNaaee/oauth/models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  email: { type: String, unique: true, required: true },
+  name: { type: String, required: true },
+  username: { type: String, unique: true },
+  photo: { type: String },
+}, { timestamp: true });
+
+module.exports = mongoose.model('User', userSchema);
